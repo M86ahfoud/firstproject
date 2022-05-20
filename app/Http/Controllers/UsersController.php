9be32e10b5fba89a5f21d61115c3d8d28cbc0273
Mahfoud <hanimahfoud@gmail.com>
2022-05-20 23:32:18 +0200

    initial commit

---
+++ app/Http/Controllers/UsersController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+
+use Illuminate\Http\Request;
+
+
+class UsersController extends Controller
+{
+
+    public function index() 
+    {
+
+        return view('index', [
+    
+            'users' => User::latest()->get(),
+        ]);
+    }
+    
+    }
---
+++ resources/views/index.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div class="action">
+
+    @foreach($users as $user)
+
+    <div>
+        <h4>{{$user->name}}</h4>
+        <h4>{{$user->email}}</h4>
+        <h4>{{$user->email_verified_at}}</h4>
+    </div>
+
+
+    @endforeach 
+
+
+    </div>
+</body>
+</html>
